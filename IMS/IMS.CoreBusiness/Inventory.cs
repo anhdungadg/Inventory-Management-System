5a18d02c9115c1e0752832a21141e0d6e352418f
Anh Dung <cscssl@gmail.com>
2022-02-09 12:38:16 +0700

    firs commit

---
+++ IMS/IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        [Required]
+        public string? InventoryName { get; set; } // this is somethings new in C# 10
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ IMS/IMS.UseCases/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,25 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        /// <summary>
+        /// IEnumerable là cái gì vậy nhỉ?
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await this.inventoryRepository.GetInventoriesByName(name);
+        }
+    }
+}
